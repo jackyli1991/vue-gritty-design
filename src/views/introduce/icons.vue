60faf21db56a69b50c734bec07cbb7b8f9a932bd
liyang <2008042226@163.com>
2026-04-15 17:57:51 +0800

    feat: 字体库示例

---
+++ src/views/introduce/icons.vue
@@ -0,0 +1,27 @@
+<!-- 图标库说明 -->
+<template>
+  <div>
+    <h3 class="font-bold border-b border-gray-300 pb-2 mb-2">图标库</h3>
+    <div class="flex flex-wrap">
+      <span
+        v-for="(icon, key) in ICONIFY_ICONS"
+        :key="key"
+        class="w-1/5 text-center p-1.5 hover:bg-gray-100 cursor-pointer rounded-md"
+      >
+        <GIcon :name="icon" />
+        <span class="w-full inline-block whitespace-nowrap text-ellipsis overflow-hidden">
+          {{ key }}
+        </span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ICONIFY_ICONS } from '@/icons'
+import { GIcon } from '@/components'
+
+defineOptions({
+  name: 'IconsLibrary',
+})
+</script>
