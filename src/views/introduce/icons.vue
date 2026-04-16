4b9f25c904533c07230705f2188559b7f3a66bc4
liyang <2008042226@163.com>
2026-04-16 08:42:51 +0800

    feat: 图标复制功能

--- src/views/introduce/icons.vue
+++ src/views/introduce/icons.vue
@@ -1,44 +1,50 @@
 <!-- 图标库说明 -->
 <template>
   <div>
-    <aFloatButton
-      type="primary"
-      icon="plus"
-      @click="modalOpen = true"
-    >
+    <aFloatButton type="primary" icon="plus" @click="modalOpen = true">
       <template #icon>
         <GIcon class="p-[0] hover:bg-transparent" :name="ICONIFY_ICONS.font" />
       </template>
     </aFloatButton>
-    <a-modal
-      v-model:open="modalOpen"
-      title="图标库(点击复制)"
-      width="80vw"
-      :footer="null">
+    <a-modal v-model:open="modalOpen" title="图标库(点击复制)" width="80vw" :footer="null">
       <div class="flex flex-wrap items-start max-h-[60vh] overflow-auto">
-      <span
-        v-for="(icon, key) in ICONIFY_ICONS"
-        :key="key"
-        class="w-[80px] text-center p-1.5 hover:bg-gray-100 cursor-pointer rounded-md"
-      >
-        <GIcon :name="icon" :size="24"/>
-        <span class="w-full inline-block whitespace-nowrap text-ellipsis overflow-hidden">
-          {{ key }}
+        <span
+          v-for="(icon, key) in ICONIFY_ICONS"
+          :key="key"
+          class="w-[80px] text-center p-1.5 hover:bg-gray-100 cursor-pointer rounded-md"
+          @click="copy(icon)"
+        >
+          <GIcon :name="icon" :size="24" />
+          <span class="w-full inline-block whitespace-nowrap text-ellipsis overflow-hidden">
+            {{ key }}
+          </span>
         </span>
-      </span>
-    </div>
+      </div>
     </a-modal>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { ICONIFY_ICONS } from '@/icons'
 import { GIcon } from '@/components'
-import { Modal as aModal, FloatButton as aFloatButton } from 'ant-design-vue'
+import { Modal as aModal, FloatButton as aFloatButton, message } from 'ant-design-vue'
+import { useClipboard } from '@vueuse/core'
 
 defineOptions({
   name: 'IconsLibrary',
+})
+
+// const text = ref('') // 要复制的内容
+const { copy, copied } = useClipboard({
+  // source: text,
+  legacy: true, // 低版本浏览器降级
+})
+
+watch(copied, (newVal) => {
+  if (newVal) {
+    message.success('复制成功')
+  }
 })
 
 const modalOpen = ref(false)
