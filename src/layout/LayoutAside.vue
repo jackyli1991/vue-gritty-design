7c468ac94a731ecad51db0dcf7404bc06bcd6c75
liyang <2008042226@163.com>
2026-04-18 09:04:00 +0800

    feat: 优化路由处理

--- src/layout/LayoutAside.vue
+++ src/layout/LayoutAside.vue
@@ -9,24 +9,21 @@
       mode="inline"
       :selected-keys="activeRoute"
       :open-keys="activeRouteParent"
-      :items="menuItems"
+      :items="accessibleRoutes"
       @select="handleSelect"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import { h } from 'vue'
 import { storeToRefs } from 'pinia'
 import router from '@/router'
-import type { RouteRecordRaw, RouteRecordNameGeneric } from 'vue-router'
-import type { ItemType, MenuProps } from 'ant-design-vue'
+import type { RouteRecordNameGeneric } from 'vue-router'
+import type { MenuProps } from 'ant-design-vue'
 import { Menu as aMenu } from 'ant-design-vue'
-import { GIcon } from '@/components'
 import logo from '@/assets/logo.png'
 import { useLayoutStore } from '@/stores/layout'
 import { useRouteStore } from '@/stores/route'
-import { ICONIFY_ICONS } from '@/icons'
 
 defineOptions({
   name: 'LayoutAside',
@@ -36,36 +33,7 @@ const layoutStore = useLayoutStore()
 const { headerHeight } = storeToRefs(layoutStore)
 
 const routeStore = useRouteStore()
-const { allRoutes, activeRoute, activeRouteParent } = storeToRefs(routeStore)
-
-// 菜单项数组
-const menuItems: ItemType[] = []
-
-/**
- * 路由配置转换为菜单项
- * @param {RouteRecordRaw[]} routes 路由配置数组
- * @param {ItemType[]} target 菜单项数组
- */
-function transformRoutesToMenuItems(routes: RouteRecordRaw[], target: ItemType[]) {
-  routes.forEach((route) => {
-    // 隐藏路由不添加到菜单中
-    if (route.meta?.hidden) {
-      return
-    }
-    const menuItem: ItemType = {
-      key: route.name as string,
-      label: route.meta?.title || route.name || '',
-      icon: () => h(GIcon, { name: ICONIFY_ICONS[route.meta?.icon as string] || '', size: 16 }),
-    }
-    if (route.children?.length) {
-      menuItem.children = []
-      transformRoutesToMenuItems(route.children, menuItem.children)
-    }
-    target.push(menuItem)
-  })
-}
-
-transformRoutesToMenuItems(allRoutes.value, menuItems)
+const { activeRoute, activeRouteParent, accessibleRoutes } = storeToRefs(routeStore)
 
 /**
  * 处理菜单项点击事件
--- src/stores/route.ts
+++ src/stores/route.ts
@@ -1,12 +1,50 @@
+import { h } from 'vue'
 import { defineStore } from 'pinia'
 import type { RouteRecordRaw } from 'vue-router'
 import type { BreadcrumbRoute } from '@/types/routeJson'
+import type { ItemType } from 'ant-design-vue'
+import { GIcon } from '@/components'
 import { autoRoutes } from '@/router/autoRoute'
 import router from '@/router'
+import { ICONIFY_ICONS } from '@/icons'
+
+interface State {
+  allRoutes: RouteRecordRaw[] // 所有路由配置
+}
+
+/**
+ * 路由权限处理
+ * 1. 隐藏路由不添加到菜单中
+ * 2. 路由权限判断，过滤没有权限的路由【to do】
+ * 3. 递归处理子路由
+ * 4. 处理菜单项的图标、标题等属性
+ * @param {RouteRecordRaw[]} routes 路由配置数组
+ * @param {ItemType[]} target 菜单项数组
+ */
+function handleRoutesPermission(routes: RouteRecordRaw[], target: ItemType[]) {
+  routes.forEach((route) => {
+    // 隐藏路由不添加到菜单中
+    if (route.meta?.hidden) {
+      return
+    }
+    const menuItem: ItemType = {
+      key: route.name as string,
+      label: route.meta?.title || route.name || '',
+      icon: () => h(GIcon, { name: ICONIFY_ICONS[route.meta?.icon as string] || '', size: 16 }),
+      // type: 'group',
+    }
+    // 子路由
+    if (route.children?.length) {
+      menuItem.children = []
+      handleRoutesPermission(route.children, menuItem?.children || [])
+    }
+    target.push(menuItem)
+  })
+}
 
 export const useRouteStore = defineStore('route', {
-  state: () => ({
-    allRoutes: autoRoutes, // 所有路由配置
+  state: (): State => ({
+    allRoutes: autoRoutes,
   }),
   getters: {
     // 当前激活的路由名称
@@ -26,10 +64,11 @@ export const useRouteStore = defineStore('route', {
       })
       return breadcrumbRoutes.slice(1) // 去掉最顶层路由，因为最顶层路由是布局路由，不是页面路由
     },
-  },
-  actions: {
-    setRoutes(routes: RouteRecordRaw[]) {
-      this.allRoutes = routes
+    // 有权限访问的路由
+    accessibleRoutes: (state) => {
+      const routes: ItemType[] = []
+      handleRoutesPermission(state.allRoutes, routes)
+      return routes
     },
   },
 })
